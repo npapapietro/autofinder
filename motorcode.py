b81a5b9f87d8b75cd4e709ce8746c6bea88ffc42
npapapietro <npapapietro@crimson.ua.edu>
2017-03-04 16:31:08 -0800

    real time editing

--- motorcode.py
+++ motorcode.py
@@ -10,52 +10,74 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-enable_pin = 18
-coil_A_1_pin = 4
-coil_A_2_pin = 17
-coil_B_1_pin = 23
-coil_B_2_pin = 24
+Ra_enable_pin = 18
+Ra_A_1_pin = 4
+Ra_A_2_pin = 17
+Ra_B_1_pin = 23
+Ra_B_2_pin = 24
 
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
+dec_enable_pin = 12
+dec_A_1_pin = 5
+dec_A_2_pin = 26
+dec_B_1_pin = 25
+dec_B_2_pin = 16
 
-GPIO.output(enable_pin, 1)
 
-def forward(delay, steps):  
+GPIO.setup(Ra_enable_pin, GPIO.OUT)
+GPIO.setup(Ra_A_1_pin, GPIO.OUT)
+GPIO.setup(Ra_A_2_pin, GPIO.OUT)
+GPIO.setup(Ra_B_1_pin, GPIO.OUT)
+GPIO.setup(Ra_B_2_pin, GPIO.OUT)
+
+GPIO.setup(dec_enable_pin, GPIO.OUT)
+GPIO.setup(dec_A_1_pin, GPIO.OUT)
+GPIO.setup(dec_A_2_pin, GPIO.OUT)
+GPIO.setup(dec_B_1_pin, GPIO.OUT)
+GPIO.setup(dec_B_2_pin, GPIO.OUT)
+
+GPIO.output(Ra_enable_pin, 1)
+GPIO.output(dec_enable_pin, 1)
+
+def forward(delay, steps, axis):  
   for i in range(0, steps):
-    setStep(1, 0, 1, 0)
+    axis(1, 0, 1, 0)
     time.sleep(delay)
-    setStep(0, 1, 1, 0)
+    axis(0, 1, 1, 0)
     time.sleep(delay)
-    setStep(0, 1, 0, 1)
+    axis(0, 1, 0, 1)
     time.sleep(delay)
-    setStep(1, 0, 0, 1)
+    axis(1, 0, 0, 1)
     time.sleep(delay)
 
-def backwards(delay, steps):  
+def backwards(delay, steps,axis):  
   for i in range(0, steps):
-    setStep(1, 0, 0, 1)
+    axis(1, 0, 0, 1)
     time.sleep(delay)
-    setStep(0, 1, 0, 1)
+    axis(0, 1, 0, 1)
     time.sleep(delay)
-    setStep(0, 1, 1, 0)
+    axis(0, 1, 1, 0)
     time.sleep(delay)
-    setStep(1, 0, 1, 0)
+    axis(1, 0, 1, 0)
     time.sleep(delay)
 
   
-def setStep(w1, w2, w3, w4):
-  GPIO.output(coil_A_1_pin, w1)
-  GPIO.output(coil_A_2_pin, w2)
-  GPIO.output(coil_B_1_pin, w3)
-  GPIO.output(coil_B_2_pin, w4)
+def dec_setStep(w1, w2, w3, w4):
+  GPIO.output(dec_A_1_pin, w1)
+  GPIO.output(dec_A_2_pin, w2)
+  GPIO.output(dec_B_1_pin, w3)
+  GPIO.output(dec_B_2_pin, w4)
+  
+def RA_setStep(w1, w2, w3, w4):
+  GPIO.output(Ra_A_1_pin, w1)
+  GPIO.output(Ra_A_2_pin, w2)
+  GPIO.output(Ra_B_1_pin, w3)
+  GPIO.output(Ra_B_2_pin, w4)  
 
 while True:
   delay = raw_input("Delay between steps (milliseconds)?")
   steps = raw_input("How many steps forward? ")
-  forward(int(delay) / 1000.0, int(steps))
+  forward(int(delay) / 1000.0, int(steps),dec_setStep)
+  forward(int(delay) / 1000.0, int(steps),RA_setStep)
   steps = raw_input("How many steps backwards? ")
-  backwards(int(delay) / 1000.0, int(steps))
+  backwards(int(delay) / 1000.0, int(steps),dec_setStep)
+  backwards(int(delay) / 1000.0, int(steps),RA_setStep)
